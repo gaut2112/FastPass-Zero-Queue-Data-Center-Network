ad36f5156ad2208e5a6f046c8eda6a86b9e28a97
Omid <a@omid.io>
2015-02-15 18:51:16 -0800

    Traffic files/Arbiter network/Evaluation scripts.

---
+++ evaluation.py
@@ -0,0 +1,174 @@
+import os
+from scapy.all import *
+import re
+import time
+
+num_host = 16
+
+g_start_time = time.time()
+
+g_correctness_list = []
+g_throughput_list = []
+g_FCT_list = []
+
+def traffic_spec(host):
+    traffic = {}
+    p = re.compile("-d h(\S*) -p (\S*) -n (\S*)")
+    traffic_file = 'traffic/' + host +'.tr'
+    for line in open(traffic_file):
+        m = p.match(line)
+        if m != None:
+            dstid = m.group(1)
+            dstport = m.group(2)
+            vol = m.group(3)
+            vol_num = int(re.findall("\d+", vol)[0])
+            if 'K' in vol:
+                vol_num *= 1000
+            elif 'G' in vol:
+                vol_num *= 10e9
+            elif 'M' in vol:
+                vol_num *= 10e6
+            traffic['10.0.0.'+dstid+':'+dstport] = vol_num
+    return traffic
+
+    
+#compare dump file with traffic spec
+def get_dump_stat(dump_file, spec):
+    global g_throughput_list
+    global g_FCT_list
+    global g_correctness_list
+    
+    stat = {}
+    pkttrace = rdpcap(dump_file)
+    for pkt in pkttrace:
+        #print pkt.time
+        #exit(1)
+        
+        if pkt.haslayer(IP) == 0 or  pkt.haslayer(UDP) == 0:
+            continue
+
+
+            
+        dst = pkt.getlayer(IP).dst
+        dport = pkt.getlayer(IP).dport
+        key = str(dst)+':'+str(dport)
+        ts = pkt.time
+        
+        if key not in spec:
+            continue
+            
+
+        if key not in stat:
+            stat[key] = {}
+            
+        # suppose you have only ONE 802.1Q layer
+        if pkt.haslayer(Dot1Q):
+            vlan = int(pkt.getlayer(Dot1Q).vlan)
+            #print vlan
+
+        if vlan not in stat[key]:
+            stat[key][vlan] = (0,ts)
+            
+        header_size = 20 # IP header
+        if pkt.proto == 17 or pkt.proto == 6:
+            header_size += 8  # L4 header
+
+            
+        stat[key][vlan] = (stat[key][vlan][0] + (pkt.len - header_size), ts)
+
+
+    for key in spec:
+        if key not in stat:
+            print "flow: " + key + " Missing"
+            g_correctness_list.append(0)
+            continue
+        flowsize = 0
+        finish_time = 0
+        for vlan in stat[key]:
+            #print stat[key][vlan]
+            flowsize += stat[key][vlan][0]
+            
+            if stat[key][vlan][1] > finish_time:
+                finish_time = stat[key][vlan][1]
+            #test
+            #print "vlan: " + key+'\t'+ str(vlan) + '\t' + str(stat[key][vlan])
+        if spec[key] == flowsize:
+            result = "OK"
+            g_correctness_list.append(1)
+        else:
+            result = "Wrong flow size"
+            g_correctness_list.append(0)
+
+        FCT = finish_time - g_start_time
+        throughput = flowsize * 8.0 / (FCT)/10e6
+    
+        print "flow " + key + '\t' + "FCT " + str(FCT) + '\t' + "thr " + str(throughput) +" Mbps\texpt " + str(spec[key]) + '\t' + "dump " + str(flowsize) + '\t' + result
+        
+        #save per flow stat
+        g_throughput_list.append(throughput)
+        g_FCT_list.append(FCT)
+		
+		
+def dump_trace_analysis():
+    global g_throughput_list
+    global g_FCT_list
+    global g_correctness_list
+    
+    for hostid in range(1,num_host + 1):
+        host = 'h' + str(hostid)
+        hostip = '10.0.0.'+str(hostid)
+
+        # retrieve traffic specification
+        traffic = traffic_spec(host)
+        
+        tracefile = 'dump/' + host + '.pcap'
+        get_dump_stat(tracefile, traffic)
+
+
+    #print overall result
+    print "=== Overall result ==="
+    print "correct flows: " + str(sum(g_correctness_list))
+    print "total flows: " + str(len(g_correctness_list))
+    print "ave throughput: " + str(sum(g_throughput_list)/len(g_throughput_list))
+    print "tail FCT: " + str(min(g_FCT_list))
+    
+
+    
+def kill_all_task():
+    cmd = "ps -ef | grep \'tcpdump\|cperf\' | grep -v grep | awk \'{print $2}\'"
+    pids = os.popen(cmd).read()
+    pid_all = ""
+    strs = pids.split('\n')
+    for pid in strs:
+        pid_all += str(pid)
+        pid_all += " "
+    #print pid_all
+    os.system("sudo kill -9 " + pid_all)
+
+if __name__=='__main__':
+
+    print "Starting tcpdump..."
+    time.sleep(1)
+    for hostid in range(1,num_host): 
+        host = 'h' + str(hostid)
+        cmd = 'mininet/util/m ' + host + ' sudo tcpdump -i ' + host +'-eth0' + ' -n -s 64 portrange 5001-5003 -w dump/' + host + '.pcap &'
+        print cmd
+        os.system(cmd)
+
+    print "Starting traffic..."
+    time.sleep(1)
+    for hostid in range(1,num_host): 
+        host = 'h' + str(hostid)
+        cmd = 'mininet/util/m ' + host + ' ./cperf trace/' + host + '.tr &'
+        print cmd
+        os.system(cmd)
+
+    #sleep sufficient amount of time
+    #change the value in need
+    time.sleep(60)
+
+    print "Terminating all task..."
+    kill_all_task()
+
+    print "Analyzing traffic statistics..."
+    dump_trace_analysis()
